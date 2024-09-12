a1844065e0934683fd4d79fb2a2854155d764b22
Tshepo <khumakotshepo1@gmail.com>
2024-09-12 23:28:41 +0200

    add to check if small screen to use useEffect on carousel when is on small divece

---
+++ src/components/useIsSmallScreen.ts
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+export function useIsSmallScreen() {
+  const [isSmallScreen, setIsSmallScreen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+
+    handleResize(); // Set initial value
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return isSmallScreen;
+}
