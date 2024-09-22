91738818ebffd608d4b9918da77cf3763151d082
Tshepo <khumakotshepo1@gmail.com>
2024-09-22 15:25:15 +0200

    remove auth check from all ships fetch functions

--- src/server/ships.server.ts
+++ src/server/ships.server.ts
@@ -1,24 +1,9 @@
-import { auth } from "@/auth";
 import { sql } from "@/database";
 
 import { cache } from "react";
 
 export const getAllShips = cache(async () => {
   try {
-    const session = await auth();
-
-    if (!session) {
-      return {
-        error: "Unauthorized",
-      };
-    }
-
-    if (session?.user?.role !== "ADMIN") {
-      return {
-        error: "Unauthorized",
-      };
-    }
-
     const { rows } = await sql.query("SELECT * FROM ships");
 
     return rows || null;
@@ -29,20 +14,6 @@ export const getAllShips = cache(async () => {
 
 export const getShipByName = cache(async (name: string) => {
   try {
-    const session = await auth();
-
-    if (!session) {
-      return {
-        error: "Unauthorized",
-      };
-    }
-
-    if (session?.user?.role !== "ADMIN") {
-      return {
-        error: "Unauthorized",
-      };
-    }
-
     const { rows } = await sql.query("SELECT * FROM ships WHERE name = $1", [
       name,
     ]);
@@ -55,20 +26,6 @@ export const getShipByName = cache(async (name: string) => {
 
 export const getAllCabins = cache(async () => {
   try {
-    const session = await auth();
-
-    if (!session) {
-      return {
-        error: "Unauthorized",
-      };
-    }
-
-    if (session?.user?.role !== "ADMIN") {
-      return {
-        error: "Unauthorized",
-      };
-    }
-
     const { rows } = await sql.query(
       "SELECT c.*, s.name as ship FROM cabins c JOIN ships s ON s.ship_id = c.ship_id"
     );
