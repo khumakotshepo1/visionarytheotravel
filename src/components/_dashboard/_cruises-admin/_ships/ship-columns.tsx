b9f2f0fbdf0fdf31a03aa09c5650664e74e89f24
Tshepo <khumakotshepo1@gmail.com>
2024-09-24 01:37:34 +0200

    add update ship form

--- src/components/_dashboard/_cruises-admin/_ships/ship-columns.tsx
+++ src/components/_dashboard/_cruises-admin/_ships/ship-columns.tsx
@@ -14,6 +14,7 @@ import Image from "next/image";
 
 import { deleteShipAction } from "@/actions/cruise.actions";
 import { toast } from "sonner";
+import { UpdateShipForm } from "./UpdateShipForm";
 
 export const shipColumns: ColumnDef<ShipPropsType>[] = [
   {
