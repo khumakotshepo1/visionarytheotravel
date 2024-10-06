f12972f73e72b9e45d3b1f4b7cee40286b19c57e
Tshepo Khumako <khumakotshepo1@gmail.com>
2024-10-06 14:51:23 +0200

    Add area-label to button and lucide icon

--- src/components/_auth/sign-button.tsx
+++ src/components/_auth/sign-button.tsx
@@ -6,10 +6,12 @@ export function SignButton() {
   return (
     <>
       <Link href="/auth/login">
-        <button className="hidden md:block px-3 py-1 rounded-full font-anton font-bold bg-orangeElement text-white">
+        <button className="hidden md:block px-4 py-1 rounded-xl font-anton font-bold bg-orangeElement text-sm text-white"
+          type="button"
+          aria-label="Login">
           Login
         </button>
-        <LogInIcon className="h-8 w-8 md:hidden" />
+        <LogInIcon className="h-8 w-8 md:hidden" aria-label="Login" />
       </Link>
     </>
   );
