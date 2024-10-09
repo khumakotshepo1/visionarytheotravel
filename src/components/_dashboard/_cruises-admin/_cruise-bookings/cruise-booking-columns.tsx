de74d10b366e709b25f2113e3d3ce96bb77ac1c7
Tshepo Khumako <khumakotshepo1@gmail.com>
2024-10-09 17:32:32 +0200

    remove imports not used

--- src/components/_dashboard/_cruises-admin/_cruise-bookings/cruise-booking-columns.tsx
+++ src/components/_dashboard/_cruises-admin/_cruise-bookings/cruise-booking-columns.tsx
@@ -4,7 +4,6 @@ import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
