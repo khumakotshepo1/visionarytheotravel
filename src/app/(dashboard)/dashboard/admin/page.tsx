5e6429864896244f1eeba11c646c91b2ce892473
Tshepo Khumako <khumakotshepo1@gmail.com>
2024-10-16 16:04:39 +0200

    add customers analytics

--- src/app/(dashboard)/dashboard/admin/page.tsx
+++ src/app/(dashboard)/dashboard/admin/page.tsx
@@ -1,6 +1,7 @@
+import { DashBookedCustomersCard } from "@/components/_dashboard/DashBookedCustomersCard";
 import { DashCruiseBookingsCard } from "@/components/_dashboard/DashCruiseBookingsCard";
-// import { getAllCustomers } from "@/server/ customer.server";
 import { getAllCruiseBookings, getPreviousCruiseTotalPrice } from "@/server/cruises.server";
+import { getAllCustomers } from "@/server/customer.server";
 
 export default async function AdminDashboardPage() {
   const cruiseBookings =
@@ -10,7 +11,7 @@ export default async function AdminDashboardPage() {
 
   console.log({ previousTotalPrice })
 
-  // const customers = (await getAllCustomers()) as CustomerPropsType[];
+  const customers = (await getAllCustomers()) as CustomerPropsType[];
 
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 place-items-center">
@@ -19,6 +20,11 @@ export default async function AdminDashboardPage() {
         link="/dashboard/admin/cruises-admin/cruise-bookings"
         data={cruiseBookings}
         prev_cruise_total_price={previousTotalPrice}
+      />
+      <DashBookedCustomersCard
+        title="Booked Customers"
+        link="/dashboard/admin/customers"
+        data={customers}
       />
     </section>
   );
