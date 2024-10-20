6d971b5e3a65252188a04e06062fec6c36c1e5b0
Tshepo Khumako <khumakotshepo1@gmail.com>
2024-10-20 09:19:26 +0200

    Add ship api

--- src/app/(root)/cruises/[cruiseSlug]/page.tsx
+++ src/app/(root)/cruises/[cruiseSlug]/page.tsx
@@ -1,5 +1,5 @@
 import { CruiseSlug } from "@/components/_cruises/CruiseSlug";
-import { getCruiseById, getCruiseDateById } from "@/server/cruises.server";
+import { getCruiseById, getCruiseDateById, getShipById } from "@/server/cruises.server";
 
 export default async function CruiseSlugPage({
   params,
@@ -9,13 +9,15 @@ export default async function CruiseSlugPage({
   const cruiseSlug = parseInt(params.cruiseSlug);
 
   const cruiseDate = (await getCruiseDateById(cruiseSlug)) as CruiseDatePropsType;
+  const ship = await getShipById(parseInt(cruiseDate.ship_id)) as ShipPropsType;
 
   console.log('Cruise Date:', cruiseDate);
+  console.log('Ship:', ship);
 
   const cruise = (await getCruiseById(Number(cruiseDate.cruise_id))) as CruisePropsType;
   return (
     <>
-      <CruiseSlug cruise={cruise} cruiseDate={cruiseDate} />
+      <CruiseSlug cruise={cruise} cruiseDate={cruiseDate} ship={ship} />
     </>
   );
 }
