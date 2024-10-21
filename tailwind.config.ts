bad467de3699e7168c68a2e8ba007443868534e1
Tshepo Khumako <khumakotshepo1@gmail.com>
2024-10-21 17:11:57 +0200

    back to nextjs14

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,33 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-  	extend: {
-  		fontFamily: {
-  			sans: ["var(--font-geist-sans)"],
-  			mono: ["var(--font-geist-mono)"],
-  			anton: ["var(--font-anton)"]
-  		},
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)',
-  			crimsonElement: 'hsl(var(--crimson-element))',
-  			darkElement: 'hsl(var(--dark-element))',
-  			lightElement: 'hsl(var(--light-element))'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
+	darkMode: ["class"],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["var(--font-geist-sans)"],
+				mono: ["var(--font-geist-mono)"],
+				anton: ["var(--font-anton)"]
+			},
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+				crimsonElement: 'hsl(var(--crimson-element))',
+				darkElement: 'hsl(var(--dark-element))',
+				lightElement: 'hsl(var(--light-element))'
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			}
+		}
+	},
+	plugins: [require("tailwindcss-animate")],
 };
 export default config;
