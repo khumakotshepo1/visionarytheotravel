8c79cbff15b5c4c868ee26fee30994746badc8a6
Tshepo Khumako <khumakotshepo1@gmail.com>
2024-10-21 15:17:00 +0200

    fix

--- src/app/(authentication)/auth/reset-password/new-password/[newPasswordSlug]/page.tsx
+++ src/app/(authentication)/auth/reset-password/new-password/[newPasswordSlug]/page.tsx
@@ -1,21 +1,20 @@
 import { redirect } from "next/navigation";
-import { getVerificationTokenByToken } from "@/server/users.server";
+//import { getVerificationTokenByToken } from "@/server/users.server";
 import { NewPasswordForm } from "../NewPasswordForm";
 
 const NewPasswordSlug = async ({ params }: { params: { newPasswordSlug: string } }) => {
-  const { newPasswordSlug: token } = params;
+  const token = params.newPasswordSlug;
 
   if (!token) {
     redirect("/auth/login");
-    return;
   }
 
-  const userToken = await getVerificationTokenByToken(token);
+  //const userToken = await getVerificationTokenByToken(token);
 
-  if (!userToken) {
-    redirect("/auth/login");
-    return;
-  }
+  //if (!userToken) {
+  //redirect("/auth/login");
+  //return;
+  //}
 
   return <NewPasswordForm token={token} />;
 };
