9fc207ce6cb474533f033af34db76bf5c239feed
Tshepo Khumako <khumakotshepo1@gmail.com>
2024-10-21 10:10:29 +0200

    add // eslint-disable-line

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -23,7 +23,8 @@ const Command = React.forwardRef<
 ))
 Command.displayName = CommandPrimitive.displayName
 
-interface CommandDialogProps extends DialogProps {}
+
+interface CommandDialogProps extends DialogProps { } // eslint-disable-line
 
 const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
